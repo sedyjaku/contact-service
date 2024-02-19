b184c7b0551cd45c5ef82a6a6d9446b3691b6667
Jakub Sedy <jakub.sedy@pwc.com>
2024-02-19 18:58:35 +0100

    Init projct

---
+++ src/test/kotlin/cz/sedy/contactservice/ContactServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.sedy.contactservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
